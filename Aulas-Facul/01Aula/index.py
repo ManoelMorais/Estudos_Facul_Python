ef54787e98f4b757dbd95bfda2795691c7a4e877
Manoel <999142818m@gmail.com>
2023-11-08 16:34:52 -0300

    up

---
+++ Aulas-Facul/01Aula/index.py
@@ -0,0 +1,21 @@
+print("Hello world!")
+print("POO")
+
+
+nome = "Manoel Morais"
+print(nome)
+
+
+numero = 1234
+print(numero)
+
+nomeDigitado = input("Whats your name:")
+print("Your name is",nomeDigitado)
+
+
+grupo = [
+    "manoel",
+    "carlos",
+    "gabriel"
+]
+print(grupo)
---
+++ Aulas-Facul/02Aula/index.py
@@ -0,0 +1,31 @@
+n1 = 6
+n2 = 9
+
+aprovado = True
+media = 7
+minino = 7
+
+print("operadores aritimetricos")
+print("soma", n1 + n2)
+print("subtração", n1 - n2)
+print("mutiplicação", n1 * n2)
+print("divisão", n1 / n2)
+print("resto", n1 % n2)
+
+print()
+
+print("operadores relacionais")
+print("igualdade", n1 == n2)
+print("diferença", n1 != n2)
+print("menor q", n1 < n2)
+print("maior q", n1 > n2)
+print("menor ou igual", n1 <= n2)
+print("maior ou igual", n1 >= n2)
+
+print()
+
+print("operadores logicos")
+print(aprovado)
+print(not aprovado) # negar se e true vira false
+print(n1 >= 7 and minino == 7)
+print(n1 >= 7 or minino == 7)
